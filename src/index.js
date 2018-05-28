a0d9026d5a6b63818be1c426abe992d6ae543754
CATALcraft1 <estesept@gmail.com>
2018-05-28 16:46:38 +0200

    ajout de dossier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Esteban/Esteban.css';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
 import Esteban from './Esteban/Esteban.jsx'
 ReactDOM.render(<Esteban />, document.getElementById('root'));
 registerServiceWorker();
